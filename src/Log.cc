826a6aa66ba9f2c899917e9b2752cf0330f3ac34
Klervie Toczé <klervie.tocze@gmail.com>
2014-06-02 10:09:01 +0200

    Added more precise timestamp

--- src/Log.cc
+++ src/Log.cc
@@ -2,6 +2,9 @@
 
 #include <cstdio>
 #include <ctime>
+#include <sys/time.h>
+#include <sstream>
+#include <iomanip>
 
 Log::Log(std::string fileName){
   mFile.open(fileName.c_str());
@@ -16,11 +19,14 @@ void Log::writeToLog(std::string message){
 }
 
 const std::string Log::currentDateTime(){
-  time_t now=time(0);
-  struct tm tstruct;
-  char buf[80];
+  timeval tv;
+  tm *tm;
+  int rc;
 
-  tstruct = *localtime(&now);
-  strftime(buf,sizeof(buf),"%Y-%m-%d.%X : ",&tstruct);
-  return buf;
+  gettimeofday(&tv, NULL);
+  tm = localtime(&tv.tv_sec);
+  
+  std::ostringstream timestamp;
+  timestamp << 1900 + tm->tm_year << "-" << std::setfill('0') << std::setw(2) << 1 + tm->tm_mon << "-" << std::setfill('0') << std::setw(2) << tm->tm_mday << "." << std::setfill('0') << std::setw(2) <<tm->tm_hour << ":" << std::setfill('0') << std::setw(2) << tm->tm_min << ":" << std::setfill('0') << std::setw(2) << tm->tm_sec << ":" << tv.tv_usec << std::endl; 
+  return timestamp.str();
 }
