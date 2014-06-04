b20e4c0227588adcb061bf418644cd395a532741
Klervie Toczé <klervie.tocze@gmail.com>
2014-06-04 09:06:36 +0200

    Made minor changes to time measurement in mme

--- src/UeContextMme.cc
+++ src/UeContextMme.cc
@@ -46,11 +46,12 @@ void UeContextMme::handleS1ApInitialContextSetupResponse(S1ApInitialContextSetup
   std::ostringstream messageLog;
   messageLog << "Message received from ENodeB: S1ApInitialContextResponse {Enb Ue S1Ap Id: " << message.enb_ue_s1ap_id() << " ERab Id: " << message.erabid() << " }" << std::endl; 
   mLog->writeToLog(messageLog.str());
+  printState();
+
   timeval endProcedure;
   gettimeofday(&endProcedure, NULL);
   messageLog << "For Ue with C-Rnti " << message.enb_ue_s1ap_id()/17 << " : end of procedure " << endProcedure.tv_usec + endProcedure.tv_sec * 1000000 << std::endl;
   mLog->writeToLog(messageLog.str());
-  printState();
 }
 
 void UeContextMme::printState(){
